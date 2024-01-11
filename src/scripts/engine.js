0173191d8182706348b8c28ca644720270d1410b
EuIngridSouza <ingridcoelhodeabreu@gmail.com>
2024-01-10 22:10:28 -0300

    Implementando Vidas ao jogador

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -3,13 +3,15 @@ const state = {
         squares: document.querySelectorAll(".square"),
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time-left"),
-        score: document.querySelector("#score")
+        score: document.querySelector("#score"),
+        lives: document.querySelector("#lives"),
     },
     values: {
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
-        currentTime: 60,
+        currentTime: 5,
+        currentLives: 3
     },
     actions: {
         timerId: setInterval(randomSquare, 1000),
@@ -17,15 +19,59 @@ const state = {
     }
 };
 
+
 function countDown() {
     state.values.currentTime--;
     state.view.timeLeft.textContent = state.values.currentTime
 
-    if (state.values.currentTime <= 0) {
-        clearInterval(state.actions.countDownTimerId)
-        clearInterval(state.actions.timerId)
-        alert("Game over! O seu resultado foi " + state.values.result)
+
+    if (state.values.currentTime < 0) {
+
+        state.values.currentLives--;
+        state.view.lives.textContent = state.values.currentLives;
+
+        if (state.values.currentLives > 0) {
+            state.values.currentTime = 5;
+            state.view.timeLeft.textContent = state.values.currentTime;
+            state.values.currentTime--;
+
+            alert("O seu resultado foi " + state.values.result + "."+"\n"+"Clique em OK para continuar");
+
+        } else {
+
+            state.values.currentTime = 0;
+            state.view.timeLeft.textContent = state.values.currentTime;
+            clearInterval(state.actions.countDownTimerId);
+            clearInterval(state.actions.timerId);
+
+            function restart(){
+                var resultado = confirm("GAME OVER!" + "\n" + "O seu resultado foi " + state.values.result + "\n" + "Clique em OK para RECOMEÇAR.");
+                if (resultado == true) {
+                    alert("Isso ai!" + "\n" + "Vamos lá!!");
+                    playSound("win");
+                    setTimeout(() => {
+                        window.location.reload();   
+                    }, 3000);
+                }
+                else{
+                    alert("Você desistiu..." + "\n" + "\n" + "Até a próxima,então!");
+                    playSound("game-over");
+                }
+            }
+
+            restart();
+        }
     }
+
+
+}
+
+
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.5;
+    audio.play();
 }
 
 function randomSquare() {
@@ -41,19 +87,19 @@ function randomSquare() {
 }
 
 
+
 function addListenerHitBox() {
     state.view.squares.forEach((square) => {
         square.addEventListener("mousedown", () => {
-            if (square.id === state.values.hitPosition) {
+            if (square.id === state.values.hitPosition && (state.values.currentLives > 0)) {
                 state.values.result++
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("hit");
             }
         })
     })
 }
-
-
 
 function initialize() {
     addListenerHitBox();
